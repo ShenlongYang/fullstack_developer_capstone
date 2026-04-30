9aae157098fe6a6b1e960fd346f4beaf16480bb7
ShenlongYang <shenlong.yang@protonmail.com>
2026-04-30 06:24:28 -0400

    Completed CarMake & CarModel models + get_cars endpoint

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
     # path for logout
     path('logout/', views.logout_request, name='logout'),
     # path for dealer reviews view
-    #path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     # path for add a review view
-    #path(route='add_review', view=views.add_review, name='add_review'),
+    path(route='add_review', view=views.add_review, name='add_review'),
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
